5eb2516aeafe03b613dfe284de9b72a3c0027d35
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-06-23 21:12:46 +0200

    add pizza/show

--- src/main/java/exercises/spring/spring_la_mia_pizzeria_crud/controller/HomeController.java
+++ src/main/java/exercises/spring/spring_la_mia_pizzeria_crud/controller/HomeController.java
@@ -2,8 +2,10 @@ package exercises.spring.spring_la_mia_pizzeria_crud.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
@@ -13,4 +15,5 @@ public class HomeController {
         // model.addAttribute("name", "java");
         return "home";
     }
+
 }
--- src/main/java/exercises/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/exercises/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -4,10 +4,13 @@ import exercises.spring.spring_la_mia_pizzeria_crud.model.Pizza;
 
 import java.util.List;
 
+import exercises.spring.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import exercises.spring.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
@@ -15,17 +18,24 @@ import exercises.spring.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.springframework.ui.Model;
 
 @Controller
-@RequestMapping
+@RequestMapping("/pizzas")
 public class PizzaController {
 
     @Autowired
     private PizzaRepository repository;
 
-    @GetMapping("/pizzas")
+    @GetMapping
     public String index(Model model) {
         List<Pizza> pizzas = repository.findAll(); // SELECT * FROM `pizzas` and give me a list of pizza objects
         model.addAttribute("pizzas", pizzas);
+        model.addAttribute("hasPizzas", !pizzas.isEmpty());
         return "pizzas/index";
 
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("pizza", repository.findById(id).get());
+        return "/pizzas/detail";
     }
 }
